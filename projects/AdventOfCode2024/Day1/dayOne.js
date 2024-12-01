8604b7eb57e9aef5b519ade56f4b52d941d2ac6f
Brady Burgener <bradylbsee@gmail.com>
2024-12-01 12:38:02 -0700

    Update dayOne.js sort

--- projects/AdventOfCode2024/Day1/dayOne.js
+++ projects/AdventOfCode2024/Day1/dayOne.js
@@ -12,15 +12,18 @@ document.getElementById('file').onchange = function() {
     var lines = text.split('\n');
     let a = [];
     let b = [];
+    let answer = 0;
     for (var line = 0; line < lines.length; line++) {
       let c = lines[line].split(' ');
       console.log(lines[line]);
       a.push(c[0]);
       b.push(c[1]);
     }
+    a = a.sort();
+    b = b.sort();
     console.log(a);
     console.log(b);
-    outputSource.innerText = (text + "\n" + a + "\n" + b);
+    outputSource.innerText = (text + "\n" + a + "\n" + b + "\n" + answer);
   };
   reader.readAsText(file);
 };
