4ab5fb7e450394ed4815386fad78b15f3b234399
Brady Burgener <bradylbsee@gmail.com>
2024-12-01 15:02:37 -0700

    Adds dayOneb.html

--- projects/AdventOfCode2024/Day1/dayOne.js
+++ projects/AdventOfCode2024/Day1/dayOne.js
@@ -1,4 +1,5 @@
 const outputSource = document.getElementById('output')
+const outputSecondSource = document.getElementById("secondOutPut")
 document.getElementById('file').onchange = function() {
   var file = this.files[0];
 
@@ -13,24 +14,36 @@ document.getElementById('file').onchange = function() {
     let a = [];
     let b = [];
     let answer = 0;
+    let answerTwo = 0;
     for (var line = 0; line < lines.length; line++) {
       let c = lines[line].trim().split(' ');
       console.log(lines[line]);
       a.push(c[0]);
-      for (let d = 1; d < c.length; d++){
-         if(!isNaN(c[d]){
-           b.push(c[d]);
-         }
+      for (let e = 1; e < c.length; e++){
+        if (c[e] != "") 
+        {
+          b.push(c[e]);
+        }
       }    
     }
     a = a.sort();
     b = b.sort();
-    console.log(a);
-    console.log(b);
     for (let d = 0; d < a.length; d++){
       answer += Math.abs(a[d] - b[d]);
     }
-    outputSource.innerText = (text + "\n" + a + "\n" + b + "\n" + answer);
+    for (let f = 0; f < a.length; f++){
+      let h = 0;
+      for(let g = 0; g < b.length; g++){
+        if(a[f] == b[g]){
+          h += 1;
+        }
+      }
+      answerTwo += h * a[f];
+    }
+    outputSecondSource.innerText = ("What is their similarity score?\nAnswer:\n" + answerTwo);
+    outputSource.innerText = ("What is the total distance between your lists?\nAnswer:\n" + answer +
+      "\n\nSorted Lists:\n" + a + "\n" + b +
+      "\n\nYour provided lists\n" + text);
   };
   reader.readAsText(file);
 };
