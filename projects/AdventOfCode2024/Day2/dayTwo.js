46cefd7ef611477d44524bf14f761b032b637cac
Brady Burgener <bradylbsee@gmail.com>
2024-12-01 23:21:56 -0700

    Adds Day 2 of AsventOfCode2024

---
+++ projects/AdventOfCode2024/Day2/dayTwo.js
@@ -0,0 +1,87 @@
+const outputSource = document.getElementById('output')
+document.getElementById('file').onchange = function() {
+  var file = this.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function() {
+    // Entire file
+    const text = this.result;
+    outputSource.innerText = text
+
+    // By lines
+    var lines = text.split('\n');
+    let a = [];
+    let b = [];
+    let answer = 0;
+    let answerTwo = 0;
+    for (var line = 0; line < lines.length; line++) {
+      a = [];
+      let c = lines[line].trim().split(' ');
+      for (let d = 0; d < c.length; d++){
+        if (c[d] != "") 
+        {
+          a.push(c[d]);
+        }
+      }
+      let test = false;
+      if (a[0] < a[1]){
+        for (let e = 1; e < c.length; e++){
+          if (Math.abs(a[0]-a[1]) >= 1 && Math.abs(a[0]-a[1]) <= 3){
+            test = true;
+          } else {
+            test = false;
+            break;
+          }
+          if(a[e-1] < a[e]){
+            if(Math.abs(a[e-1] - a[e]) >= 1 &&  Math.abs(a[e-1] - a[e]) <= 3){
+              test = true;
+            } else {
+              test = false;
+              break;
+            }
+            test = true;
+          } else {
+            test = false;
+            break;
+          }
+          test = true;
+        }
+      } else if (a[0] > a[1]){
+        for (let e = 1; e < c.length; e++){
+          if (Math.abs(a[0]-a[1]) >= 1 && Math.abs(a[0]-a[1]) <= 3){
+            test = true;
+          } else {
+            test = false;
+            break;
+          }
+          if(a[e-1] > a[e]){
+            if(Math.abs(a[e-1] - a[e]) >= 1 &&  Math.abs(a[e-1] - a[e]) <= 3){
+              test = true;
+            } else {
+              test = false;
+              break;
+            }
+          } else {
+            test = false;
+            break;
+          }
+          test = true;
+        }
+      } else {
+        test = false;
+      }
+      b.push(test);
+      if (test == true) {
+        answer += 1;
+      }
+      if (test == false){
+        answerTwo += 1;
+      }
+    }
+    
+    outputSource.innerText = ("Number of Safe reports?\nAnswer:\n" + answer + " " + answerTwo +
+      "\n\nAnswer List:\n" + b +
+      "\n\nYour provided lists\n" + text);
+  };
+  reader.readAsText(file);
+};
