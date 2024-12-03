e03873ae81e31275cbe1fa5bab1a29ee73040a62
Brady Burgener <bradylbsee@gmail.com>
2024-12-03 11:48:14 -0700

    updates Magic8Ball

--- projects/Magic8Ball/Magic8Ball.js
+++ projects/Magic8Ball/Magic8Ball.js
@@ -9,24 +9,43 @@ let question = (prompt("What do you want to ask the magic 8 ball"));
 // Display a randomly chosen response
 let randomNum = Math.floor(Math.random() * responses.length);
 let randomTime = Math.floor(Math.random() * 6);
+let lastNumberGiven = Math.max(randomTime+1,2);
 const questionCustomer = document.getElementById("questionCustomer");
 const answerDelay = document.getElementById("answerDelay");
 const answerCustomer = document.getElementById("answerCustomer");
+const askAgain = document.getElementById("askAgain");
 questionCustomer.innerHTML = ("<h2>" + question + "</h2>");
-// this should be on the second line
-if (randomTime <= 1){
-    answerDelay.innerHTML = ("<p>This one is too easy... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
-} else if (randomTime <= 2) {
-    answerDelay.innerHTML = ("<p>This one is easy... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
-}else if (randomTime <= 3) {
-    answerDelay.innerHTML = ("<p>This one is a little harder... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
-}else if (randomTime <= 4) {
-    answerDelay.innerHTML = ("<p>This one is starting to get hard... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
-}else {
-    answerDelay.innerHTML = ("<p>This one is hard... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
-};
-//this should be on the third line
+
+timeDelay();
 setTimeout(thisIsHard, 1000 * randomTime);
+
+// this should be on the second line
+function timeDelay() {
+    if (randomTime <= 1){
+        answerDelay.innerHTML = ("<p>This one is too easy... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
+    } else if (randomTime <= 2) {
+        answerDelay.innerHTML = ("<p>This one is easy... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
+    }else if (randomTime <= 3) {
+        answerDelay.innerHTML = ("<p>This one is a little harder... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
+    }else if (randomTime <= 4) {
+        answerDelay.innerHTML = ("<p>This one is starting to get hard... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
+    }else {
+        answerDelay.innerHTML = ("<p>This one is hard... <strong>Can you give me " + randomTime + " seconds</strong>.</p>");
+    };
+}
+//this should be on the third line
 function thisIsHard(){
     answerCustomer.innerHTML = ("<p>Magic 8 Ball says... <strong>" + responses[randomNum] + "</strong>.</p>");
 }
+function randomNumbers(){
+    randomNum = Math.floor(Math.random() * responses.length);
+    randomTime = Math.floor(Math.random() * lastNumberGiven);
+    lastNumberGiven = Math.max(randomTime+1,2);
+}
+function askSecondTime(){
+    randomNumbers();
+    timeDelay();
+    answerCustomer.innerHTML = ("<p>Magic 8 Ball says... <strong></strong>.</p>");
+    setTimeout(thisIsHard, 1000 * randomTime);
+};
+askAgain.addEventListener("click",askSecondTime);
